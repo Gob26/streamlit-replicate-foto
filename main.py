1ecbc91ec8b1384f76d29cb18ceeaf41d733b1a5
gob26 <profservis26@yandex.ru>
2024-05-09 10:44:27 +0300

    Добавил введение промта, макс мин и шаг для высоты и ширины изображения

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ st.markdown("# :rainbow[AI II Image]")
 def configure_sitebar():
     with st.sidebar:
         with st.form('my form'):
-            width = st.number_input('Ширина изображения')
-            height = st.number_input('Высота изображения')
+            width = st.number_input('Ширина изображения', min_value=256, max_value=2048,value=1024, step=16)
+            height = st.number_input('Высота изображения',min_value=256, max_value=2048,value=1024, step=16)
+            promt = st.text_area('Введите ПРОМТ для создания изображения')
             submitted = st.form_submit_button("Создать", type="primary")
 def main():
     configure_sitebar()
