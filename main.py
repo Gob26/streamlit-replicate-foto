3cba45b7111023d810138ecbbc05a328ee3ab04e
gob26 <profservis26@yandex.ru>
2024-05-09 10:51:22 +0300

    Добавил ретерн данных из функции

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def configure_sitebar():
             height = st.number_input('Высота изображения',min_value=256, max_value=2048,value=1024, step=16)
             promt = st.text_area('Введите ПРОМТ для создания изображения')
             submitted = st.form_submit_button("Создать", type="primary")
+
+        return {
+            "width": width,
+        "height": height,
+        "promt": promt,
+        "submitted": submitted,
+        }
 def main():
     configure_sitebar()
 
