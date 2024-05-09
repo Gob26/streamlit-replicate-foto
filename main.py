73d09ce09f871872d9858ba916d0a31dade47121
gob26 <profservis26@yandex.ru>
2024-05-09 10:27:25 +0300

    Добавил формы ширина и высота изображения

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.markdown("# :rainbow[AI II Image]")
+
+
+def configure_sitebar():
+    with st.sidebar:
+        with st.form('my form'):
+            width = st.number_input('Ширина изображения')
+            height = st.number_input('Высота изображения')
+
+def main():
+    configure_sitebar()
+
+
+if __name__=="__main__":
+    main()
